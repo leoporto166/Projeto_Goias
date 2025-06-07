fe54d57f2dac9a2f1fea3826c01d797fdbbe116e
leoporto166 <leonardo.porto166@gmail.com>
2025-06-06 23:44:20 -0300

    Criação carrossel PG

--- goias_app/src/Pages/Home/index.js
+++ goias_app/src/Pages/Home/index.js
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
-import logo from "../../img/logo-goias-esporte-clube-256.png";
+
 import { useState } from "react";
 import "./home.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+import logo from "../../img/logo-goias-esporte-clube-256.png";
+import card1 from "../../img/tadeu_ramom.jpeg"
+import card2 from "../../img/benitez.webp"
+import card3 from "../../img/ingressos-goxvolt.png"
+import card4 from "../../img/torcida.png"
+
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 function Home() {
   const [menuActive, setMenuActive] = useState(false);
@@ -67,10 +80,70 @@ function Home() {
       </header>
       <div className="divider2"></div>
       <main>
-        <div className="noticias">
-            <h1>Goias vence atletico-go em classico emocionante</h1>
+        
+        <div className="title-not">
+          <h1>Noticias</h1>
         </div>
-      
+        
+        <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        pagination = {{clickable: true}}
+
+        className="noticias"
+        >
+          <SwiperSlide>
+            <div className="card-principal">
+              <img src = {card1}></img>
+              <span className="text">Goias vence classico emocionante contra Atlético-go</span>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+    
+        <Swiper 
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={30}
+        slidesPerView={1.5}
+        navigation
+        pagination = {{clickable: true}}
+        //autoplay = {{delay: 5000}}
+        breakpoints={{
+          320: {  
+            slidesPerView: 1.5,
+          },
+          640:{
+            slidesPerView: 1.3,
+          },
+          1024:{
+            slidesPerView: 2.5,
+          },
+          1440:{
+            slidesPerView: 3.5,
+          },
+        }}
+        className="noticias">
+
+        
+       
+        <SwiperSlide>
+          <div className="card"><img src={card3}></img>
+          <span className="text">Ingressos a venda:  Goias x Volta R.</span>
+          
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="card"><img src={card2}></img>
+          <span className="text">Anúncio Ofical: Martin Benítez</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="card">
+            <img src={card4}></img>
+            <span className="text">Contamos com seu apoio</span>
+            </div>
+        </SwiperSlide>
+     
+       </Swiper>
       </main>
     </div>
     
