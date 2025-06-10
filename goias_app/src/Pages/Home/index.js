fe690297c2730e61bbf4c37690d1ef6f2123cd05
leoporto166 <leonardo.porto166@gmail.com>
2025-06-09 21:58:44 -0300

    Socio, partidas, menu PG

--- goias_app/src/Pages/Home/index.js
+++ goias_app/src/Pages/Home/index.js
@@ -15,6 +15,10 @@ import card3 from "../../img/ingressos-goxvolt.png"
 import card4 from "../../img/torcida.png"
 
 import crb from "../../img/crb.png"
+import atle from "../../img/athletic.png"
+import chape from "../../img/chape.png"
+
+import socio from "../../img/socio.png"
 
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
@@ -183,13 +187,46 @@ function Home() {
             
             <div className="info">
                 <h1>Campeonato brasileiro serie B</h1>
-                <h2 id="txt">11º rodada</h2>
+                <h2 id="txt">12º rodada</h2>
 
               <div className="svg-info">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#114440c4" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                 </svg>
-                <h3 id="txt">Domingo, Jun 08, 16h00</h3>
+                <h3 id="txt">Domingo, Jun 14, 16h00</h3>
+              </div>
+
+              <div className="svg-info">
+                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#114440c4"
+                class="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <path strokeLinecap="round"  strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                
+                </svg>
+                <h3 id="txt">Estadido Rei Pelé</h3>
+              </div>
+
+              <div className="button">
+                
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="part-icon">
+            <img src={logo}></img>
+            <img src={atle}></img>
+            
+            <div className="info">
+                <h1>Campeonato brasileiro serie B</h1>
+                <h2 id="txt">13º rodada</h2>
+
+              <div className="svg-info">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#114440c4" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                </svg>
+                <h3 id="txt">Domingo, Jun 23, 21h00</h3>
               </div>
 
               <div className="svg-info">
@@ -212,7 +249,7 @@ function Home() {
         <SwiperSlide>
           <div className="part-icon">
             <img src={logo}></img>
-            <img src={crb}></img>
+            <img src={chape}></img>
             
             <div className="info">
                 <h1>Campeonato brasileiro serie B</h1>
@@ -222,7 +259,7 @@ function Home() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#114440c4" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                 </svg>
-                <h3 id="txt">Domingo, Jun 08, 16h00</h3>
+                <h3 id="txt">Domingo, Jun 29, 19h00</h3>
               </div>
 
               <div className="svg-info">
@@ -232,44 +269,11 @@ function Home() {
                 <path strokeLinecap="round"  strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                 
                 </svg>
-                <h3 id="txt">Estadido Haile Pinheiro</h3>
+                <h3 id="txt">Arena Condá </h3>
               </div>
 
               <div className="button">
-                <button>Comprar ingresso</button>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="part-icon">
-            <img src={logo}></img>
-            <img src={crb}></img>
-            
-            <div className="info">
-                <h1>Campeonato brasileiro serie B</h1>
-                <h2 id="txt">11º rodada</h2>
-
-              <div className="svg-info">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#114440c4" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
-                </svg>
-                <h3 id="txt">Domingo, Jun 08, 16h00</h3>
-              </div>
-
-              <div className="svg-info">
-                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#114440c4"
-                class="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                <path strokeLinecap="round"  strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                 
-                </svg>
-                <h3 id="txt">Estadido Haile Pinheiro</h3>
-              </div>
-
-              <div className="button">
-                <button>Comprar ingresso</button>
               </div>
             </div>
           </div>
@@ -277,6 +281,18 @@ function Home() {
           
         </SwiperSlide>
        </Swiper>
+
+       <div className="dividir">
+
+       </div>
+
+       <div className="socio-img">
+        <Link>
+        <img src={socio}></img>
+        </Link>
+        
+       </div>
+
       </main>
     </div>
     
