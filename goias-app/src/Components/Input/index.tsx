ceabe5279dc2f9b7bd9bb6f0981d299668a7a8e8
leoporto166 <leonardo.porto166@gmail.com>
2025-06-29 18:12:27 -0300

    teste2-telaCadastro

--- goias-app/src/Components/Input/index.tsx
+++ goias-app/src/Components/Input/index.tsx
@@ -5,7 +5,8 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement>{}
 
 export function Input(props: InputProps){
     return(
-        <input className="h-9 bg-white rounded-md outline-none px-2" 
+        <input className="px-2 py-2 h-10 rounded-md outline-none mb-3" 
+        style={{background: "#A8BFB0" }}
         {...props}
         />
     )
--- goias-app/src/pages/Cadastro/index.tsx
+++ goias-app/src/pages/Cadastro/index.tsx
@@ -1,17 +1,52 @@
 import { Input } from "../../Components/Input"
 import logo  from "../../Assets/logo-goias-esporte-clube-256.png"
+import { useState } from "react"
 
 export default function Cadastro(){
-    return(
-        <main className=" relative flex flex-col items-center justify-center h-screen w-full">
-            <img src={logo} alt="Escudo Goias"
-            className="absolute w-100 h-100 opacity-50 z-0 "
-            ></img>
 
-            <div className=" flex flex-col bg-green-200 h-5/12 w-9/12 rounded-xl px-2 z-100">
-                h1
-                <Input></Input>
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    return(
+        <div className="flex flex-col items-center h-screen w-full ">
+            <div className=" h-2 w-full"></div>
+            <div className="h-24">
+                <img src={logo} alt="Escudo Goias"
+                    className="w-20"
+                
+                ></img>
             </div>
-        </main>
+
+
+                <h1 className="text-4xl font-semibold mb-90">CADASTRO</h1>
+
+            <form className=" flex flex-col 
+            text-xl w-full max-w-11/12 px-2 gap-2">
+                <label className="">Nome Completo</label>
+                <Input
+                type="text"
+                placeholder="Seu nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                ></Input>
+
+                <label>Email</label>
+                <Input
+                type="text"
+                placeholder="Seu Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+
+                ></Input>
+
+                <label>Senha</label>
+                <Input
+                type="password"
+                placeholder="Sua senha aqui"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                ></Input>
+            </form>
+        </div>
     )
 }
