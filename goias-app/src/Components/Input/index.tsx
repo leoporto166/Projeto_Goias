a77f3fba337aa8714756c34d5d21017535a03abe
leoporto166 <leonardo.porto166@gmail.com>
2025-06-30 12:43:19 -0300

    Update index.tsx

--- goias-app/src/Components/Input/index.tsx
+++ goias-app/src/Components/Input/index.tsx
@@ -5,7 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement>{}
 
 export function Input(props: InputProps){
     return(
-        <input className="px-2 py-2 h-10 rounded-md outline-none mb-3" 
+        <input className="p-2 h-10 rounded-md outline-none mb-3" 
         style={{background: "#A8BFB0" }}
         {...props}
         />
