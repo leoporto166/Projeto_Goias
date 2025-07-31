21faaae6bcf127f64fe1c72ae7d7784c271bcca9
leoporto166 <leonardo.porto166@gmail.com>
2025-07-30 21:56:14 -0300

    Header v.2 tamanho tela grande

--- goias-app/src/Components/Header/header.tsx
+++ goias-app/src/Components/Header/header.tsx
@@ -13,6 +13,8 @@ import { BiLogOut } from "react-icons/bi"
 import { onAuthStateChanged } from "firebase/auth";
 import { signOut } from "firebase/auth";
 
+import { FaTimes } from "react-icons/fa";
+
 interface HeaderProps{
   txt: string;
 }
@@ -59,12 +61,12 @@ export function Header(){
 
 
     const header: HeaderProps[] =[
-      {txt: "Noticias"},
-      {txt: "Esmeralda"},
-      {txt: "Loja"},
-      {txt: "Clube"},
-      {txt: "Base"},
-      {txt: "Elenco"},
+      {txt: "NOTICIAS"},
+      {txt: "ESMERALDA"},
+      {txt: "LOJA"},
+      {txt: "CLUBE"},
+      {txt: "BASE"},
+      {txt: "ELENCO"},
     ]
 
     const menu: HeaderProps[] =[
@@ -111,7 +113,7 @@ export function Header(){
     };
     return(
     <div className="flex flex-col justify-center items-center">
-        <header className="w-full h-[120px] relative flex items-center px-2 gap-4 max-w-7xl justify-center">
+        <header className="w-full h-[120px] relative flex items-center px-2 gap-4 justify-center">
           
             <div className="relative w-[140px] h-[110px] sm:w-[135px]  md:w-[130px] lg:w-[120px]">
               <img
@@ -120,68 +122,31 @@ export function Header(){
                 className="absolute w-full h-full z-10 mt-10"
               />
             </div>
-            <div className="flex gap-6 h-12/12 items-end mb-8 w-full relative">
+            <div className="flex gap-6 h-12/12 items-end mb-8 w-full relative ml-4 text-lg font-bold ">
 
               <div
-                className={`mobile-menu ${menuActive ? "active" : ""}, absolute start-0 flex gap-6`}
-                onClick={toggleMenu}>
-                {header.map((txt, index) => (
-                  <div key={`menu-${index}`} onClick={() => setSelectedIndex(index)} className="cursor-pointer select-none">
-                    {txt.txt}
-                  </div>
-                ))}
-              </div>
-              {selectedIndex === 0 && (
-                <ul className={`nav-list ${menuActive ? "active" : ""}`}>
-                  {menuN.map((noticia, subIndex) => (
-                    <li key={`noticia-${subIndex}`}>{noticia.txt}</li>
-                  ))}
-                </ul>
-              )}
-              {selectedIndex === 1 && (
-                <ul className={`nav-list ${menuActive ? "active" : ""}`}>
-                  {menuE.map((esmeralda, subIndex) => (
-                    <li key={`noticia-${subIndex}`}>{esmeralda.txt}</li>
-                  ))}
-                </ul>
-              )}
-              {selectedIndex === 2 && (
-                <ul className={`nav-list ${menuActive ? "active" : ""}`}>
-                  {menuL.map((loja, subIndex) => (
-                    <li key={`noticia-${subIndex}`}>{loja.txt}</li>
-                  ))}
-                </ul>
-              )}
-              {selectedIndex === 3 && (
-                <ul className={`nav-list ${menuActive ? "active" : ""}`}>
-                  {menuC.map((clube, subIndex) => (
-                    <li key={`noticia-${subIndex}`}>{clube.txt}</li>
-                  ))}
-                </ul>
-              )}
-              {selectedIndex === 4 && (
-                <ul className={`nav-list ${menuActive ? "active" : ""}`}>
-                  {menuB.map((base, subIndex) => (
-                    <li key={`noticia-${subIndex}`}>{base.txt}</li>
-                  ))}
-                </ul>
-              )}
-              {selectedIndex === 5 && (
-                <ul className={`nav-list ${menuActive ? "active" : ""}`}>
-                  {menuEl.map((elenco, subIndex) => (
-                    <li key={`noticia-${subIndex}`}>{elenco.txt}</li>
-                  ))}
-                </ul>
-              )}
+              className={`mobile-menu ${menuActive ? "active" : ""} absolute start-0 flex gap-6 text-gray-500`}
+              onClick={toggleMenu}
+            >
+              {header.map((txt, index) => (
+                <div
+                  key={`menu-${index}`}
+                  onClick={() => setSelectedIndex(index)}
+                  className={`menu cursor-pointer select-none ${selectedIndex === index ? "text-green-950" : ""}`}
+                >
+                  {txt.txt}
+                </div>
+              ))}
+            </div>
             
             
             
             
-              <div className="login absolute end-0 cursor-pointer mb-[-5px] ">
+              <div className="login absolute end-0 cursor-pointer mb-[-5px] text-black ">
                 {logado ? (
                   <div
                   >
-                      <button onClick={handleLogOut} className="signin-btn cursor-pointer "><BiLogOut size={26} /></button>
+                      <button onClick={handleLogOut} className="signin-btn cursor-pointer"><BiLogOut size={26} /></button>
                   </div>
                 ): (
                   <div className="login">
@@ -191,11 +156,71 @@ export function Header(){
               </div>
             
             </div>
+
+             
         </header>
+
+        
           
         <div className="w-full h-[1px] bg-green-800">
             
         </div>
+
+            {selectedIndex === 0 && (
+                    <ul className={`nav-list ${selectedIndex === 0 ? "zero" : ""}`}>
+                      {menuN.map((noticia, subIndex) => (
+                        <li key={`noticia-${subIndex}`}>{noticia.txt}</li>
+                      ))}
+                    </ul>
+                  )}
+                  {selectedIndex === 1 && (
+                    <ul className={`nav-list ${selectedIndex === 1 ? "um" : ""}`}>
+                      {menuE.map((esmeralda, subIndex) => (
+                        <li key={`noticia-${subIndex}`}>{esmeralda.txt}</li>
+                      ))}
+                    </ul>
+                  )}
+                  {selectedIndex === 2 && (
+                    <ul className={`nav-list ${selectedIndex === 2? "dois" : ""}`}>
+                      {menuL.map((loja, subIndex) => (
+                        <li key={`noticia-${subIndex}`}>{loja.txt}</li>
+                      ))}
+                    </ul>
+                  )}
+                  {selectedIndex === 3 && (
+                    <ul className={`nav-list ${selectedIndex === 3 ? "tres" : ""}`}>
+                      {menuC.map((clube, subIndex) => (
+                        <li key={`noticia-${subIndex}`}>{clube.txt}</li>
+                      ))}
+                    </ul>
+                  )}
+                  {selectedIndex === 4 && (
+                    <ul className={`nav-list ${selectedIndex === 4 ? "quatro" : ""}`}>
+                      {menuB.map((base, subIndex) => (
+                        <li key={`noticia-${subIndex}`}>{base.txt}</li>
+                      ))}
+                    </ul>
+                  )}
+                  {selectedIndex === 5 && (
+                    <ul className={`nav-list ${selectedIndex === 5 ? "cinco" : ""}`}>
+                      {menuEl.map((elenco, subIndex) => (
+                        <li key={`noticia-${subIndex}`}>{elenco.txt}</li>
+                      ))}
+                    </ul>
+                  )}
+                  {selectedIndex !== -1 ? (
+                  <FaTimes
+                  className="ml-[98%] cursor-pointer z-1000"
+                  size={20}
+                  onClick={() => {setSelectedIndex(-1)
+                    setMenuActive(false)
+                  } }></FaTimes>
+                  ) : (
+                    <></>
+                  )
+                }
+                         
+        
     </div>
     )
 }
