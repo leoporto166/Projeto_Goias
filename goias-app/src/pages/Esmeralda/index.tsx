9c0a3adef3835494c44c33a318ba99852de798a6
leoporto166 <leonardo.porto166@gmail.com>
2025-08-26 21:58:58 -0300

    att Esmeralda

--- goias-app/src/pages/Esmeralda/index.tsx
+++ goias-app/src/pages/Esmeralda/index.tsx
@@ -4,7 +4,9 @@ import { Header } from "../../Components/Header/header";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../../services/firebaseconnection";
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FaArrowDown } from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
 
 
 
@@ -12,6 +14,7 @@ interface SocioProps {
     descricao: string;
     informacao1: string;
     informacao2: string;
+    informacao3: string;
     preco: string;
     titulo: string;
     button: string;
@@ -28,15 +31,9 @@ export function Esmeralda(){
 
     const [socio, setSocio] = useState<SocioProps[]>([])
     const [socioImg, setSocioImg] = useState<SocioImgProps[]>([])
-    const [largura, setLargura] = useState(window.innerWidth);
 
     useEffect(() => {
 
-        function atualizarLargura() {
-            setLargura(window.innerWidth);
-        }
-
-        window.addEventListener("resize", atualizarLargura);
 
         const socioRef  = collection(db, "Socio")
         const queryRef = query(socioRef, orderBy("preco", "asc"))
@@ -48,9 +45,10 @@ export function Esmeralda(){
                 const data = doc.data()
 
                 lista.push({
-                    descricao: data.descricao,
+                    descricao: data.descrição,
                     informacao1: data.informacao1,
                     informacao2: data.informacao2,
+                    informacao3: data.informacao3,
                     preco: data.preco,
                     titulo: data.titulo,
                     button: data.button,
@@ -97,15 +95,7 @@ export function Esmeralda(){
             <Header />
 
             <section>
-            { largura < 700 ? (
-                socioImg.slice(1).map((img, index) => (
-                <div key={index}>
-                    <div>
-                    <img src={img.img} alt={`socio-${index}`} />
-                    </div>
-                </div>
-                ))
-            ) : (
+            { 
                 socioImg.slice(1).map((img, index) => (
                 <div key={index}>
                     <div
@@ -113,33 +103,36 @@ export function Esmeralda(){
                     backgroundImage: `linear-gradient(rgba(0,35,0,0.7), rgba(0,25,0,0.7), rgba(0,0,0,1)), url(${img.img})`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
-                    width: "100%",
-                    height: "60vh",
                 }}
+                className="w-[100%] h-[60vh] lg:h-[50vh]"
                     >
-                        <div className="flex flex-col justify-center items-center w-[100%] h-[50vh] text-white">
+                        <div className="flex flex-col justify-center items-center w-[100%] h-[50vh] text-white ">
                             <div className="">
-                                <h2 className="text-3xl">
+                                <h2 className="text-2xl px-2 sm:text-3xl">
                                     QUEM É ESMERALDINO É
                                 </h2>
 
-                                <h1 className="text-7xl font-bold">
+                                <h1 className="text-6xl font-bold px-2 sm:text-7xl">
                                     ESMERALDA
                                 </h1>
 
-                                <h3 className="mt-5 text-xl">FAÇA PARTE DO MAIOR DO CENTRO-OESTE</h3>
+                                <h3 className="mt-5 text-lg px-2 sm:text-xl">FAÇA PARTE DO MAIOR DO CENTRO-OESTE</h3>
 
-                                <h4 className="text-center mt-10 font-bold text-lg">CONHEÇA OS NOVOS PLANOS</h4>
+                                <div className="text-center mt-10 font-bold text-lg flex flex-col justify-center items-center">
+
+                                    <h4 className="">CONHEÇA OS NOVOS PLANOS</h4>
+
+                                    <FaArrowDown className="mt-10"></FaArrowDown>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 ))
 
-            )
             }
 
-                <div className="bg-black w-[100%] h-[50vh]">
+                <div className="bg-black w-[100%] h-[60vh] mt-[-50px]">
                 <Swiper
                 modules={[Navigation, Pagination, Autoplay]}
                 slidesPerView={1}
@@ -180,10 +173,49 @@ export function Esmeralda(){
                 {
                     socio.map((socio) => (
                         <SwiperSlide key={socio.id} className="px-2 flex flex-col justify-center items-center">
-                            <div className="bg-white w-[250px] h-[300px] text-center py-2">
-                                <div>
+                            <div className="w-[300px] flex flex-col justify-center items-center py-2 bg-white rounded-xl">
+
+                                <div className="text-2xl font-semibold">
                                     {socio.titulo}
                                 </div>
+
+                                <div className="py-2">
+                                    <h3 className="font-normal">{socio.descricao}</h3>
+                                </div>
+
+                                <div className="p-2 border rounded border-green-600/30 font-bold">
+                                    {Number(socio.preco).toLocaleString("pt-BR", {
+                                        style: "currency",
+                                        currency: "BRL"
+                                    })}
+                                </div>
+                                
+
+                                    <div className="py-2 text-[15px] px-2 flex flex-col justify-start items-start">
+
+                                        <div className="flex justify-center items-center gap-1">
+                                            <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                            <h3 className="py-2 text-sm">{socio.informacao1}</h3>
+                                        </div>
+                                        <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+
+                                        <div className="flex justify-center items-center gap-1">
+                                            <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                            <h3 className="py-2 text-sm">{socio.informacao2}</h3>
+                                        </div>
+                                        <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+
+                                        <div className="flex justify-center items-center gap-1">
+                                            <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                            <h3 className="py-2 text-sm">{socio.informacao3}</h3>
+                                        </div>
+                                        <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+
+                                    </div>
+                                
+                                    <div className="flex justify-center bg-green-700 my-2 rounded text-green-50 mb-4 cursor-pointer py-1  text-center  hover:bg-white hover:text-green-500 border border-green-500 transition duration-500 w-[98%] mt-25">
+                                        {socio.button}
+                                    </div>
                             </div>
                         </SwiperSlide>
                     ))
