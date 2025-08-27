1efe666d7efe8235cc59b2559180387fc9cdb0c4
leoporto166 <leonardo.porto166@gmail.com>
2025-08-27 17:27:50 -0300

    Update index.tsx

--- goias-app/src/pages/Esmeralda/index.tsx
+++ goias-app/src/pages/Esmeralda/index.tsx
@@ -15,6 +15,9 @@ interface SocioProps {
     informacao1: string;
     informacao2: string;
     informacao3: string;
+    informacao4: string;
+    informacao5: string;
+    informacao6: string;
     preco: string;
     titulo: string;
     button: string;
@@ -36,7 +39,7 @@ export function Esmeralda(){
 
 
         const socioRef  = collection(db, "Socio")
-        const queryRef = query(socioRef, orderBy("preco", "asc"))
+        const queryRef = query(socioRef, orderBy("id", "asc"))
 
         const unsub = onSnapshot(queryRef, (snapshot) => {
             let lista = [] as SocioProps[]
@@ -49,6 +52,9 @@ export function Esmeralda(){
                     informacao1: data.informacao1,
                     informacao2: data.informacao2,
                     informacao3: data.informacao3,
+                    informacao4: data.informacao4,
+                    informacao5: data.informacao5,
+                    informacao6: data.informacao6,
                     preco: data.preco,
                     titulo: data.titulo,
                     button: data.button,
@@ -62,7 +68,7 @@ export function Esmeralda(){
         })
 
         const socioImgRef = collection(db, "SocioImg")
-        const queryRefImg = query(socioImgRef, orderBy("id", "asc"))
+        const queryRefImg = query(socioImgRef, orderBy("id", "desc"))
 
         const unsubImg = onSnapshot(queryRefImg, (snapshot) => {
 
@@ -131,7 +137,7 @@ export function Esmeralda(){
 
             }
 
-                <div className="bg-black w-[100%] h-[60vh] mt-[-50px]">
+                <div className="bg-black w-[100%] h-[100vh] mt-[-50px]">
                 <Swiper
                 modules={[Navigation, Pagination, Autoplay]}
                 slidesPerView={1}
@@ -144,11 +150,11 @@ export function Esmeralda(){
                     spaceBetween: 25
                   },
                   640:{
-                    slidesPerView: 1.3,
+                    slidesPerView: 1.5,
                     spaceBetween:20
                   }, 
                   641:{
-                    slidesPerView: 1.5
+                    slidesPerView: 1.7
                     
                   },
                   760:{
@@ -169,56 +175,100 @@ export function Esmeralda(){
                 }} className="w-full"
                >
 
-                {
-                    socio.map((socio) => (
-                        <SwiperSlide key={socio.id} className="px-2 flex flex-col justify-center items-center">
-                            <div className="w-[300px] flex flex-col justify-center items-center py-2 bg-white rounded-xl">
-
-                                <div className="text-2xl font-semibold">
-                                    {socio.titulo}
-                                </div>
-
-                                <div className="py-2">
-                                    <h3 className="font-normal">{socio.descricao}</h3>
-                                </div>
-
-                                <div className="p-2 border rounded border-green-600/30 font-bold">
-                                    {Number(socio.preco).toLocaleString("pt-BR", {
-                                        style: "currency",
-                                        currency: "BRL"
-                                    })}
-                                </div>
-                                
-
-                                    <div className="py-2 text-[15px] px-2 flex flex-col justify-start items-start">
-
-                                        <div className="flex justify-center items-center gap-1">
-                                            <FaCheckCircle color="green" size={12}></FaCheckCircle>
-                                            <h3 className="py-2 text-sm">{socio.informacao1}</h3>
+                <div className="">
+                    {
+                        socio.map((socio) => (
+                            <SwiperSlide key={socio.id} className="px-2 flex flex-col justify-center items-center">
+                                <div className="w-[280px] flex flex-col justify-center items-center py-2 bg-white rounded-t-xl mb-[-2px]">
+                                    <div className="text-2xl font-semibold">
+                                            {socio.titulo}
                                         </div>
-                                        <div className="w-[98%] h-[0.5px] bg-green-800"></div>
-
-                                        <div className="flex justify-center items-center gap-1">
-                                            <FaCheckCircle color="green" size={12}></FaCheckCircle>
-                                            <h3 className="py-2 text-sm">{socio.informacao2}</h3>
-                                        </div>
-                                        <div className="w-[98%] h-[0.5px] bg-green-800"></div>
-
-                                        <div className="flex justify-center items-center gap-1">
-                                            <FaCheckCircle color="green" size={12}></FaCheckCircle>
-                                            <h3 className="py-2 text-sm">{socio.informacao3}</h3>
-                                        </div>
-                                        <div className="w-[98%] h-[0.5px] bg-green-800"></div>
-
+                                        <div className="py-2">
+                                            <h3 className="font-normal text-green-900/80">{socio.descricao}</h3>
                                     </div>
-                                
-                                    <div className="flex justify-center bg-green-700 my-2 rounded text-green-50 mb-4 cursor-pointer py-1  text-center  hover:bg-white hover:text-green-500 border border-green-500 transition duration-500 w-[98%] mt-25">
-                                        {socio.button}
+                                    <div className="p-2 border rounded border-green-600/30 font-bold">
+                                        {Number(socio.preco).toLocaleString("pt-BR", {
+                                            style: "currency",
+                                            currency: "BRL"
+                                        })}
                                     </div>
-                            </div>
-                        </SwiperSlide>
-                    ))
-                }
+                                </div>
+                                <div className="w-[280px] flex flex-col h-[500px] items-center py-2 bg-white text-[16px]
+                                ">
+                                        <div className="py-2 px-2 flex flex-col justify-start items-start">
+                                            <div className="flex justify-center items-center gap-2">
+                                                <div className="w-[10px]">
+                                                    <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                                </div>
+                    
+                                                <h3 className="py-2">{socio.informacao1}</h3>
+                                            </div>
+                                            <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+                                            <div className="flex justify-center items-center gap-2">
+                                                <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                                <h3 className="py-2">{socio.informacao2}</h3>
+                                            </div>
+                                            <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+                    
+                                            <div className="flex justify-center items-center gap-2">
+                    
+                                            <div className="w-[10px]">
+                                                <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                            </div>
+                    
+                                            <h3 className="py-2">{socio.informacao3}</h3>
+                                            </div>
+                                            <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+                                            {
+                                                socio.informacao4 && (
+                                                  <div>
+                                                    <div className="flex justify-center items-center gap-2">
+                                                    <div className="w-[10px]">
+                                                        <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                                    </div>
+                                                    <h3 className="py-2">{socio.informacao4}</h3>
+                                                    </div>
+                                                    <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+                                                  </div>
+                                                )
+                                            }
+                                             {
+                                                socio.informacao5 && (
+                                                  <div>
+                                                    <div className="flex justify-center items-center gap-2">
+                                                    <div className="w-[10px]">
+                                                        <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                                    </div>
+                                                    <h3 className="py-2">{socio.informacao5}</h3>
+                                                    </div>
+                                                    <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+                                                  </div>
+                                                )
+                                            }
+                                             {
+                                                socio.informacao6 && (
+                                                  <div>
+                                                    <div className="flex justify-center items-center gap-2">
+                                                    <div className="w-[10px]">
+                                                        <FaCheckCircle color="green" size={12}></FaCheckCircle>
+                                                    </div>
+                                                    <h3 className="py-2">{socio.informacao6}</h3>
+                                                    </div>
+                                                    <div className="w-[98%] h-[0.5px] bg-green-800"></div>
+                                                  </div>
+                                                )
+                                            }
+                                        </div>
+                                </div>
+                                <div className="w-[280px] flex flex-col py-2 bg-white mt-[-2px] rounded-b-xl justify-center items-center h-[50px]">
+                                    <div className="flex justify-center bg-green-700 my-2 rounded text-green-50 mb-4 cursor-pointer py-1  text-center  hover:bg-white hover:text-green-500 border border-green-500 transition duration-500 w-[98%]">
+                                            {socio.button}
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </div>
                 
                     
 
